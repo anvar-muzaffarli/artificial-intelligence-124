71a832c7a4598685d828aa7231213c92edd24d8d
anvar-muzaffarli <prof.anvar2013@gmail.com>
2023-01-25 20:28:13 +0400

    Navbar yarimciq yaradildi

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import shekil from '../assets/images/logo.svg'
+import './Navbar.css'
+function Navbar() {
+    // JSX elementleri icerisinde deyishen {} icerisinde yazilir
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src={shekil}/></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+
+      <li class="nav-item">
+      <a class="nav-link" href="#">Blog</a>
+    </li>
+
+    <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+ 
+   
+     
+       
+      </ul>
+    <a class="btn btn-primary">Sign In</a>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
